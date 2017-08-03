0d360f21407d81be35ee863f5ac2be778742e096
yeyuqiudeng <yeyuqiudeng@gmail.com>
2017-08-03 11:50:57 +0800

    test: test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <vue-alert :visible.sync="visible" title="弹窗标题" content="文本内容" buttonText="测试点击"></vue-alert>
-    <p @click="visible = true">open alert</p>
+    <p @click="visible = true">打开</p>
   </div>
 </template>
 
